aa53ee9995c10620d227f0d5f52e16e493cc2497
Rucodeby <73294566+Rucodeby@users.noreply.github.com>
2023-12-21 08:41:17 +0500

    Initial Commit

---
+++ Society/AuthWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Society {
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window {
+
+        Random random = new Random();
+        public AuthWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+            if ( Captha.CaptchaText != null && CapthaBox.Text.ToUpper() != Captha.CaptchaText ) {
+                MessageBox.Show("Wrong captcha!");
+                CapthaBox.Text = "";
+                Captha.CreateCaptcha(random.Next(4, 9));
+                ButtonDisable();
+                return;
+            }
+
+            User user = dbEntities.GetContext().User.ToList().Find(u => u.Login.Trim() == TextBox.Text.Trim() && u.Password.Trim() == PasswordBox.Password.Trim());
+            if (user != null) {
+                MessageBox.Show("Авторизация прошла успешно!");
+                MainWindow.user = user;
+                var window = new MainWindow();
+                window.Show();
+                this.Close();
+            } else if (CapthaPanel.Visibility == Visibility.Collapsed) {
+                MessageBox.Show("Логин или пароль неверны!");
+                CapthaPanel.Visibility = Visibility.Visible;
+                Captha.CreateCaptcha(random.Next(4, 9));
+            } else {
+                MessageBox.Show("Логин или пароль неверны!");
+                ButtonDisable();
+                CapthaBox.Text = "";
+                Captha.CreateCaptcha(random.Next(4, 9));
+            }
+                
+
+        }
+
+        private async void ButtonDisable() {
+            LogInBut.IsEnabled = false;
+            await Task.Delay(10000);
+            LogInBut.IsEnabled = true;
+        }
+
+
+    }
+
+
+}
---
+++ Society/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Society {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public static User user;
+        public MainWindow() {
+            InitializeComponent();
+            
+
+            if (user != null) {
+                NameLabel.Visibility = Visibility.Visible;
+                LogOutBut.Visibility = Visibility.Visible;
+                LogInBut.Visibility = Visibility.Collapsed;
+                NameLabel.Content = user.Name.Trim() + " " + user.SecondName.Trim();
+                if (user.IsAdmin)
+                    UserTab.Visibility = Visibility.Visible;
+            }
+            
+            ProductList.ItemsSource = dbEntities.GetContext().Product.ToList();
+            DividentList.ItemsSource = dbEntities.GetContext().Dividend.ToList();
+            MembershipFeeList.ItemsSource = dbEntities.GetContext().MembershipFee.ToList();
+            UserList.ItemsSource = dbEntities.GetContext().User.ToList();
+        }
+
+        private void LogInBut_Click(object sender, RoutedEventArgs e) {
+            var window = new AuthWindow();
+            this.Close();
+            window.Show();
+        }
+
+        private void LogOutBut_Click(object sender, RoutedEventArgs e) {
+            NameLabel.Visibility= Visibility.Collapsed;
+            LogOutBut.Visibility = Visibility.Collapsed;
+            UserTab.Visibility = Visibility.Collapsed;
+            LogInBut.Visibility = Visibility.Visible;
+            user = null;
+        }
+    }
+}
